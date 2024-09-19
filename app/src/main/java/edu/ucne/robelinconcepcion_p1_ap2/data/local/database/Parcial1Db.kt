4af27048b2403861790874c1d7ca593545be0ba7
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-09-19 01:29:41 -0400

    Agrega la database

---
+++ app/src/main/java/edu/ucne/robelinconcepcion_p1_ap2/data/local/database/Parcial1Db.kt
@@ -0,0 +1,15 @@
+package edu.ucne.robelinconcepcion_p1_ap2.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.robelinconcepcion_p1_ap2.data.local.dao.AlgoDao
+import edu.ucne.robelinconcepcion_p1_ap2.data.local.entities.AlgoEntity
+
+@Database(
+    version = 1,
+    exportSchema = false,
+    entities = [AlgoEntity::class]
+)
+abstract class Parcial1Db: RoomDatabase() {
+    abstract fun algoDao(): AlgoDao
+}
