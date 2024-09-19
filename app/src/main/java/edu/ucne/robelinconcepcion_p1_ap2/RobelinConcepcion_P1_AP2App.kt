d56fe68d25f7cc32969fc1f63b10441c08b91cce
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-09-19 00:57:49 -0400

    Agrega Hilt configuration

---
+++ app/src/main/java/edu/ucne/robelinconcepcion_p1_ap2/RobelinConcepcion_P1_AP2App.kt
@@ -0,0 +1,9 @@
+package edu.ucne.robelinconcepcion_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RobelinConcepcion_P1_AP2App: Application(){
+
+}
