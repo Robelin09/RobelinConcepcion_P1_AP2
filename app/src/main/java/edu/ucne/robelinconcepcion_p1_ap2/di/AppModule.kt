a0c6debe3454a4a02864b7962bdd9f8ea9bbc4be
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-09-21 15:04:03 -0400

    Agrega el AppModule

---
+++ app/src/main/java/edu/ucne/robelinconcepcion_p1_ap2/di/AppModule.kt
@@ -0,0 +1,31 @@
+package edu.ucne.robelinconcepcion_p1_ap2.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.robelinconcepcion_p1_ap2.data.local.database.Parcial1Db
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideParcial1DB(
+        @ApplicationContext appContext: Context
+    ) = Room.databaseBuilder(
+        appContext,
+        Parcial1Db::class.java,
+        "Parcial.Db"
+    ).fallbackToDestructiveMigration()
+        .build()
+
+
+    @Provides
+    @Singleton
+    fun provideAlgoDao(parcial1Db: Parcial1Db) = parcial1Db.algoDao()
+}
